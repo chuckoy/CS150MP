38f2f5f926b57b2c6f91e2ea727e8d49eee228cf
konsenne <meeko.konsenne>
2015-05-28 02:40:31 +0800

    Textbox and Filler Interpretation

--- proto2.py
+++ proto2.py
@@ -1,5 +1,9 @@
 import Tkinter
 import sys
+from Tkinter import *
+from ScrolledText import *
+import tkFileDialog
+import tkMessageBox
 
 class MenuBar(Tkinter.Menu):
     def __init__(self, parent):
@@ -7,20 +11,35 @@ class MenuBar(Tkinter.Menu):
         fileMenu = Tkinter.Menu(self, tearoff=False)
         self.add_cascade(label="File",underline=0, menu=fileMenu)
         fileMenu.add_command(label="New File", underline=0, command=self.quit)        
-        fileMenu.add_command(label="Open File", underline=0, command=self.quit)
+        fileMenu.add_command(label="Open File", underline=0, command=self.open_command)
         fileMenu.add_command(label="Save", underline=0,command=self.quit)
         fileMenu.add_command(label="Save As", underline=5,command=self.quit)          
         fileMenu.add_command(label="Exit", underline=0, command=self.quit)
         self.add_command(label="Run", underline=0, command=self.quit)
-    def quit(self):
+
+    def open_command(self):
+        # file = tkFileDialog.askopenfile(parent=root,mode='rb',title='Select a file')
+        # if file != None:
+        #     contents = file.read()
+        #     textPad.insert('1.0',contents)
+        #     file.close()
         sys.exit(0)
 
+    def quit(self):
+        sys.exit(0)
+    
 class App(Tkinter.Tk):
     def __init__(self):
         Tkinter.Tk.__init__(self)
+        textPad=ScrolledText(master=self,wrap='word',width=120,height=20)
+        interpreter=ScrolledText(master=self,wrap='word',width=120,height=20)
         menubar = MenuBar(self)
+        
         self.config(menu=menubar)
-
+        #textPad.grid(row=0,column=0)
+        #interpreter.grid(row=1,column=0)
+        textPad.pack(fill='both',expand=True,padx=3,pady=3)
+        interpreter.pack(fill='both',expand=True,padx=3,pady=3)
 if __name__ == "__main__":
     app = App()
     app.title('Tanders Programming Language') 
