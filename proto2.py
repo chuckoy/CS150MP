8610231095f6d4daf1406fa82a53a9df9e6510da
Rowel Ventura <venturaaard@gmail.com>
2015-05-28 06:29:32 +0800

    putanginang save_command

--- proto2.py
+++ proto2.py
@@ -4,16 +4,18 @@ from Tkinter import *
 from ScrolledText import *
 import tkFileDialog
 import tkMessageBox
+import os
 
 class MenuBar(Tkinter.Menu):
     def __init__(self, parent,textPad):
         Tkinter.Menu.__init__(self, parent)
         self.textPad=textPad
+        self.file= ""
         fileMenu = Tkinter.Menu(self, tearoff=False)
         self.add_cascade(label="File",underline=0, menu=fileMenu)
         fileMenu.add_command(label="New File", underline=0, command=self.new_command)        
         fileMenu.add_command(label="Open File", underline=0, command=self.open_command)
-        fileMenu.add_command(label="Save", underline=0,command=self.quit)
+        fileMenu.add_command(label="Save", underline=0,command=self.save_command)
         fileMenu.add_command(label="Save As", underline=5,command=self.saveas_command)          
         fileMenu.add_command(label="Exit", underline=0, command=self.quit)
         self.add_command(label="Run", underline=0, command=self.quit)
@@ -23,25 +25,38 @@ class MenuBar(Tkinter.Menu):
             answer=tkMessageBox.askyesno('Save File!','Do you want to save file?')
             if answer == True:
                 self.saveas_command()
-        self.textPad.delete('1.0', END) 
+        self.textPad.delete('1.0', END)
     
     def open_command(self):
-        file = tkFileDialog.askopenfile(parent=self,mode='rb',title='Select a file')
-        if file != None:
+        self.file = tkFileDialog.askopenfile(parent=self,mode='rb',title='Select a file')
+        if self.file != None:
             #print "fucker"
-            contents = file.read()
+            contents = self.file.read()
             self.textPad.insert('1.0',contents)
             #print contents
-            file.close()
+            self.file.close()
 
     def saveas_command(self):
-        file = tkFileDialog.asksaveasfile(mode='w', defaultextension=".txt")
-        if file is None: # asksaveasfile return `None` if dialog closed with "cancel".
+        self.file = tkFileDialog.asksaveasfile(mode='w', defaultextension=".txt")
+        if self.file is None: # asksaveasfile return `None` if dialog closed with "cancel".
             return
         save_file = str(self.textPad.get(1.0, END)) # starts from `1.0`, not `0.0`
-        file.write(save_file)
-        file.close()
-        
+        self.file.write(save_file)
+        self.file.close()
+
+    def save_command(self):
+        if self.file == "":
+            self.saveas_command()
+        else:
+            g = str(self.file).split(" ")
+            h = g[2].split("\'")
+            filename =  h[1]
+
+            file = open(filename, 'w')
+            save_file = str(self.textPad.get(1.0, END))
+            file.write(save_file)
+            file.close()
+
     def quit(self):
         sys.exit(0)
     
