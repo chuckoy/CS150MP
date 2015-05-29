eb6bf5c21e478a6e1a8e9043538b00b0e21ff830
Chuck Logan Lim <chucky_da_great@yahoo.com>
2015-05-29 18:48:21 +0800

    Integrated hotfix
    GUI left

--- prog_lang.py
+++ prog_lang.py
@@ -4,6 +4,7 @@ import io
 from dictionaries import dictionaries
 from lexical_analyser import lexical_analyser
 from slr_parser import slr_parser
+from semantic_analyzer import SemanticAnalyzer
 
 class ProgLang:
 	def __init__( self, inFileName, outFileName = "a.out" ):
@@ -13,7 +14,6 @@ class ProgLang:
 		self.dicts = dictionaries()
 		self.lexical = lexical_analyser( inFileName, self.dicts )
 		self.parser = slr_parser( self.dicts )
-		#self.semantic = SemanticsAnalyser()
 		self.runProgram()
 
 	def runProgram( self ):
@@ -22,13 +22,18 @@ class ProgLang:
 
 		# insert logic for running program here
 		self.tokens = self.lexical.run()
-		self.parsed = self.parser.run( self.tokens )
+		#self.parsed = self.parser.run( self.tokens )
 		hotfix = []
 		for expr in self.tokens:
 			subHotfix = []
 			for code, proc, raw in expr:
 				tokenTry = values.index( code )
-				subHotfix.append( [ raw, keys[ tokenTry ] ] )
+				second = keys[ tokenTry ]
+				if second == "INT_LIT" or second == "FLOAT_LIT":
+					second = "CONSTNUM"
+				subHotfix.append( [ raw, second ] )
 			hotfix.append( subHotfix )
+		self.semantic = SemanticAnalyzer( types = self.dicts.DTYPES, content = hotfix )
+		self.semantic.analyze_block()
 
 progLang = ProgLang( sys.argv[ 1 ] )
